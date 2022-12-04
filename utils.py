eb323b9fbe3884aba7aaf67f9183c729270cd58c
fadel <fadelsougou@gmail.com>
2022-12-04 23:46:39 +0100

    feat Ornstein Uhlenbeck Likelihood function formula

--- utils.py
+++ utils.py
@@ -9,3 +9,22 @@ def W_process(T, N):
     incr = np.insert(incr, 0, 0)
     return incr.cumsum()
 
+
+# returns an array containing the integral between 0 and t of func(s)dWs for t in [0, T/N, 2T/N, ..., T]
+def dW_integral(func, T, N): 
+    dW = W_increments(T, N)
+    time_steps = np.linspace(0, T, N+1)[:-1]
+    f = func(time_steps)
+    ans = f*dW
+    ans = np.insert(ans, 0, 0)
+    return ans.cumsum()
+
+#returns an array conataining [x0, x1, ..., xN] a sample of an OU_process at times [0, T/N, 2T/N, ..., T] 
+def OU_process(T, N, x0, alpha, beta, gamma): 
+    def exp_alpha(t):
+        return np.exp(-alpha*t)
+
+    time_steps = np.linspace(0, T, N+1)
+
+    return x0*exp_alpha(time_steps) + gamma*(1-exp_alpha(time_steps)) + beta*exp_alpha(time_steps)*dW_integral(exp_alpha, T, N)
+
